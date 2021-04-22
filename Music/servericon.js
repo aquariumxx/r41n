fe8d6368328f3308f4d9d944897b4bc3002c43a1
Wolf721 <79043462+wolfwolf12@users.noreply.github.com>
2021-04-23 01:06:00 +0300

    Create servericon.js

---
+++ Music/servericon.js
@@ -0,0 +1,22 @@
+const Discord = require('discord.js');
+
+
+module.exports = {
+        name: 'avatar',
+        description: 'Show user avatar',
+        aliases: ["icon", "servericon", "avatarserver"],
+    async execute(message, args) {
+    
+        execute(message, args) {
+        const { guild } = message
+        let icon = guild.iconURL({size: 4096, dynamic: true})
+
+        const servericonEmbed = new Discord.MessageEmbed()
+        .setDescription(`Server icon for ${guild.name}:`)
+        .setImage(icon)
+        .setFooter(`Requested by ${message.author.tag}`)
+        .setTimestamp();
+
+        message.channel.send(servericonEmbed)
+    }
+}
