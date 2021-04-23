1fa9c79d7d0ec1886dcd4875f0d3464eccee635b
Wolf721 <79043462+wolfwolf12@users.noreply.github.com>
2021-04-23 23:42:30 +0300

    Update setup.bat

--- setup.bat
+++ setup.bat
@@ -1,1 +1,1 @@
-Enpm install node.js @discordjs/opus discord.js ffmpeg-static figlet lyrics-finder string-progressbar ytdl-core discord-ytdl-core youtube-sr
+npm install node.js @discordjs/opus discord.js ffmpeg-static figlet lyrics-finder string-progressbar ytdl-core discord-ytdl-core youtube-sr
