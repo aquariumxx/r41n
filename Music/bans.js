5abbb46fbf45cccfddadebba70fd182b8af36038
Wolf721 <79043462+wolfwolf12@users.noreply.github.com>
2021-04-24 17:36:01 +0300

    Update bans.js

--- Music/bans.js
+++ Music/bans.js
@@ -11,7 +11,7 @@ async execute(message, args) {
         message.guild.fetchBans().then(bans => {
             let bansembed = new MessageEmbed()
                 .setTitle(`**Ban's**`)
-                .setColor("")
+                .setColor("#FF0000")
                 .setDescription(`**Banned User**: ${bans.size}`)
                 .setFooter("Status")
                 .setTimestamp()
