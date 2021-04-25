ad4f9cb1d59fbe595149c6a86e8c87c7a42d2e2f
Wolf721 <79043462+wolfwolf12@users.noreply.github.com>
2021-04-25 20:07:47 +0300

    Update index.js

--- index.js
+++ index.js
@@ -836,7 +836,7 @@ client.on("guildMemberAdd", member => {
     .setTitle(
       `**Welcome**`
     )
-    .setColor("#FF0000")
+    .setColor("RANDOM")
     .setThumbnail(client)
     .addField(
       "┃**Name** : ",
@@ -844,14 +844,39 @@ client.on("guildMemberAdd", member => {
     .addField(
       "┃**Welcome**",
       `Welcome to the server, 
-      ${member}`)
+${member}`)
     .addField(
-      "┃**User** :",
-      "**[" + `${member.id}` + "]**")
+      "┃**ID User** :",
+      "**" + `${member.id}` + "**")
     .addField(
       "┃**Your are the member**",
       `${member.guild.memberCount}`)
     .addField("Server", `${member.guild.name}`, true)
+    .setFooter(`**${member.guild.name}**`)
+    .setTimestamp()
+    .setImage(
+      "https://cdn.discordapp.com/attachments/756491435229839410/756491623592099890/image0-14.gif"
+    )
+    .setFooter(`${member.guild.name}`)
+    .setTimestamp();
+  channel.send(joinembed);
+});
+
+client.on("guildMemberRemove", member => {
+  const channel = member.guild.channels.cache.find(
+    channel => channel.name === "left"
+  );
+  let client = member.user.avatarURL();
+  if (!channel) return;
+  const joinembed = new Discord.MessageEmbed()
+    .setTitle(
+      `**Left**`
+    )
+    .setColor("RANDOM")
+    .setThumbnail(client)
+    .addField("┃**Name** : ", `${member}`)
+    .addField("┃**All Member** :", `${member.guild.memberCount}`)
+    .addField("┃**Server Name** :", `${member.guild.name}`, true)
     .setFooter(`**${member.guild.name}**`)
     .setTimestamp()
     .setImage(
