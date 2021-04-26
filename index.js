5aa808e6ee92e3cf39235692772bab359f788ea8
Wolf721 <79043462+wolfwolf12@users.noreply.github.com>
2021-04-27 02:59:48 +0300

    Update index.js

--- index.js
+++ index.js
@@ -242,6 +242,19 @@ if(message.content.startsWith(`${prefix}musicbot`)){
     message.channel.send(embed)
   }
 
+//An code announcement for everyone but no one knows so fine ^w^
+if(message.content.startsWith(`${prefix}musicbot`)){
+    //define saymsg
+    const saymsg = message.content.slice(Number(prefix.length) + 5)
+    //define embed
+     message.react("<:emoji_4:815583574983966720>").catch(console.error);
+    const embed = new Discord.MessageEmbed()
+    .setDescription("**Click Here Code Lock:** **[Code Lock](https://pastebin.com/b7eR4JNF)**")
+    .setColor("#FF0000")
+    //send the Message
+    message.channel.send(embed)
+  }
+
 //command Handler DO NOT TOUCH
  const prefixRegex = new RegExp(`^(<@!?${client.user.id}>|${escapeRegex(prefix)})\\s*`);
  if (!prefixRegex.test(message.content)) return;
