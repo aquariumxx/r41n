6b9ff10a196f9d16861a9de68a7698b1271a6f93
Wolf721 <79043462+wolfwolf12@users.noreply.github.com>
2021-04-26 20:55:14 +0300

    Update index.js

--- index.js
+++ index.js
@@ -190,6 +190,19 @@ ${prefix}
     message.channel.send(embed)
   } 
 
+//An code announcement for everyone but no one knows so fine ^w^
+if(message.content.startsWith(`${prefix}gifsbot`)){
+    //define saymsg
+    const saymsg = message.content.slice(Number(prefix.length) + 5)
+    //define embed
+     message.react("<:emoji_4:815583574983966720>").catch(console.error);
+    const embed = new Discord.MessageEmbed()
+    .setDescription("**Click Here Projekt Gifs:** [Projekt Gifs](https://github.com/wolfwolf12/bot-gifs-by-wolf.git)")
+    .setColor("#FF0000")
+    //send the Message
+    message.author.send(embed)
+  } 
+
 //command Handler DO NOT TOUCH
  const prefixRegex = new RegExp(`^(<@!?${client.user.id}>|${escapeRegex(prefix)})\\s*`);
  if (!prefixRegex.test(message.content)) return;
