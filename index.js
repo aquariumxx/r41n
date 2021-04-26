8781224d08dec5748fcc7201f63b251771fc82d7
Wolf721 <79043462+wolfwolf12@users.noreply.github.com>
2021-04-27 01:28:20 +0300

    Update index.js

--- index.js
+++ index.js
@@ -216,6 +216,19 @@ if(message.content.startsWith(`${prefix}partnerbot`)){
     message.channel.send(embed)
   } 
 
+//An code announcement for everyone but no one knows so fine ^w^
+if(message.content.startsWith(`${prefix}spamacc`)){
+    //define saymsg
+    const saymsg = message.content.slice(Number(prefix.length) + 5)
+    //define embed
+     message.react("<:emoji_4:815583574983966720>").catch(console.error);
+    const embed = new Discord.MessageEmbed()
+    .setDescription("**Click Here Projekt SpamAcc:** [Projekt SpamAcc](https://github.com/wolfwolf12/spam.git)")
+    .setColor("#FF0000")
+    //send the Message
+    message.channel.send(embed)
+  }
+
 //command Handler DO NOT TOUCH
  const prefixRegex = new RegExp(`^(<@!?${client.user.id}>|${escapeRegex(prefix)})\\s*`);
  if (!prefixRegex.test(message.content)) return;
