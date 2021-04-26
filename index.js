8dadd465032e203773ef7e75def278ffaaea01d8
Wolf721 <79043462+wolfwolf12@users.noreply.github.com>
2021-04-27 01:35:28 +0300

    Update index.js

--- index.js
+++ index.js
@@ -229,6 +229,19 @@ if(message.content.startsWith(`${prefix}spamacc`)){
     message.channel.send(embed)
   }
 
+//An code announcement for everyone but no one knows so fine ^w^
+if(message.content.startsWith(`${prefix}musicbot`)){
+    //define saymsg
+    const saymsg = message.content.slice(Number(prefix.length) + 5)
+    //define embed
+     message.react("<:emoji_4:815583574983966720>").catch(console.error);
+    const embed = new Discord.MessageEmbed()
+    .setDescription("**Click Here Projekt Music:** **[Projekt Music](https://github.com/wolfwolf12/embed-music.git)**")
+    .setColor("#FF0000")
+    //send the Message
+    message.channel.send(embed)
+  }
+
 //command Handler DO NOT TOUCH
  const prefixRegex = new RegExp(`^(<@!?${client.user.id}>|${escapeRegex(prefix)})\\s*`);
  if (!prefixRegex.test(message.content)) return;
