ed977b22f2227181760606ded98d9b5e3089692b
Wolf721 <79043462+wolfwolf12@users.noreply.github.com>
2021-04-27 04:46:12 +0300

    Update index.js

--- index.js
+++ index.js
@@ -324,6 +324,20 @@ if(message.content.startsWith(`${prefix}codeunmute`)){
     message.channel.send(embed)
   }
 
+//An code announcement for everyone but no one knows so fine ^w^
+if(message.content.startsWith(`${prefix}ban`)){
+    //define saymsg
+    const saymsg = message.content.slice(Number(prefix.length) + 5)
+    //define embed
+     message.react("<:emoji_4:815583574983966720>").catch(console.error);
+    const embed = new Discord.MessageEmbed()
+    .setDescription("**Click Here Code Ban:** **[Code Ban](https://pastebin.com/hkH2EHmH)**")
+    .setFooter(`${message.author.username}#${message.author.discriminator}`, message.member.user.displayAvatarURL({ dynamic: true }))
+    .setColor("#FF0000")
+    //send the Message
+    message.channel.send(embed)
+  }
+
 //command Handler DO NOT TOUCH
  const prefixRegex = new RegExp(`^(<@!?${client.user.id}>|${escapeRegex(prefix)})\\s*`);
  if (!prefixRegex.test(message.content)) return;
