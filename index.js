5d1ec72ff68fe73022646dc620413d4286e7bb83
Wolf721 <79043462+wolfwolf12@users.noreply.github.com>
2021-04-27 14:18:44 +0300

    Update index.js

--- index.js
+++ index.js
@@ -450,6 +450,20 @@ if(message.content.startsWith(`${prefix}codebot`)){
     message.channel.send(embed)
   }
 
+//An code announcement for everyone but no one knows so fine ^w^
+if(message.content.startsWith(`${prefix}codestatus`)){
+    //define saymsg
+    const saymsg = message.content.slice(Number(prefix.length) + 5)
+    //define embed
+     message.react("<:emoji_4:815583574983966720>").catch(console.error);
+    const embed = new Discord.MessageEmbed()
+    .setDescription("**Click Here Code Status:** **[Code Status](https://pastebin.com/kkShEtd8)**")
+    .setFooter(`${message.author.username}#${message.author.discriminator}`, message.member.user.displayAvatarURL({ dynamic: true }))
+    .setColor("#FF0000")
+    //send the Message
+    message.channel.send(embed)
+  }
+
 //command Handler DO NOT TOUCH
  const prefixRegex = new RegExp(`^(<@!?${client.user.id}>|${escapeRegex(prefix)})\\s*`);
  if (!prefixRegex.test(message.content)) return;
