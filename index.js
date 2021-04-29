36f2044b894b2f6f1793ec26d35949978f0acce1
Wolf721 <79043462+wolfwolf12@users.noreply.github.com>
2021-04-29 22:22:06 +0300

    Update index.js

--- index.js
+++ index.js
@@ -1245,6 +1245,36 @@ client.on("guildMemberRemove", member => {
   channel.send(joinembed);
 });
 
+client.on('message', message => {
+if (message.content.startsWith(prefix + 'list emoji')) {
+  let Emojis = "";
+    let EmojisAnimated = "";
+    let EmojiCount = 0;
+    let Animated = 0;
+    let OverallEmojis = 0;
+    function Emoji(id) {
+      return client.emojis.cache.get(id).toString();
+    }
+    message.guild.emojis.cache.forEach((emoji) => {
+      OverallEmojis++;
+      if (emoji.animated) {
+        Animated++;
+        EmojisAnimated += Emoji(emoji.id);
+      } else {
+        EmojiCount++;
+        Emojis += Emoji(emoji.id);
+      }
+    });
+    let Embed = new Discord.MessageEmbed()
+      .setTitle(`Emojis in ${message.guild.name}.`)
+      .setDescription(`
+        **Animated [${Animated}]**:\n${EmojisAnimated}\n\n**Standard [${EmojiCount}]**:\n${Emojis}\n\n**All Emoji [${OverallEmojis}]**
+      `)
+      .setColor(`RANDOM`);
+    message.channel.send(Embed);
+  
+}})
+
 function delay(delayInms) {
  return new Promise(resolve => {
    setTimeout(() => {
