74d0a62e8edb7fb27551227a00bcfb48f1cae786
Wolf721 <79043462+wolfwolf12@users.noreply.github.com>
2021-05-19 20:04:31 +0300

    Update help.js

--- others/help.js
+++ others/help.js
@@ -50,7 +50,7 @@ module.exports = {
 > \`.pcouple\` - \`.panimal\` - \`.panime\`
 > \`.pemoji\`
 **Links**
-[support](https://discord.gg/DXJVbuuJdq) - [invite](https://discord.com/api/oauth2/authorize?client_id=806840212608909344&permissions=8&scope=bot)
+**[Support](https://discord.gg/DXJVbuuJdq) - [invite](https://discord.com/api/oauth2/authorize?client_id=806840212608909344&permissions=8&scope=bot)**
 `)
 
    .setFooter(`Requested by ${message.author.username}#${message.author.discriminator}`, message.member.user.displayAvatarURL({ dynamic: true }))
