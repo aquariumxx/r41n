7a278329e1ca2b383375fe26d7d1ac584fc6393c
Wolf721 <79043462+wolfwolf12@users.noreply.github.com>
2021-05-19 20:03:17 +0300

    Update game.js

--- others/game.js
+++ others/game.js
@@ -22,7 +22,7 @@ module.exports = {
 **Game Commands**
 > \`.slap\` - \`.hug\`
 **Links**
-[support](https://discord.gg/DXJVbuuJdq) - [invite](https://discord.com/api/oauth2/authorize?client_id=806840212608909344&permissions=8&scope=bot)
+**[Support](https://discord.gg/DXJVbuuJdq) - [invite](https://discord.com/api/oauth2/authorize?client_id=806840212608909344&permissions=8&scope=bot)**
 `)
 
    .setFooter(`Requested by ${message.author.username}#${message.author.discriminator}`, message.member.user.displayAvatarURL({ dynamic: true }))
