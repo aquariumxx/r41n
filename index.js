4815ddb1560041dc512b8fd1f5b5be94e3d557af
Wolf721 <84997879+Wolf-721@users.noreply.github.com>
2021-06-03 18:00:50 +0300

    Update index.js

--- index.js
+++ index.js
@@ -636,6 +636,36 @@ client.on("guildDelete", guild => {
   channel.send(embed);
 });
 
+client.on('message', message => {
+if (message.content.startsWith(prefix + 'list emoji')) {
+  let Emojis = "";
+    let EmojisAnimated = "";
+    let EmojiCount = 0;
+    let Animated = 0;
+    let OverallEmojis = 0;
+    function Emoji(id) {
+      return client.emojis.cache.get(id).toString();
+    }
+    message.guild.emojis.cache.forEach((emoji) => {
+      OverallEmojis++;
+      if (emoji.animated) {
+        Animated++;
+        EmojisAnimated += Emoji(emoji.id);
+      } else {
+        EmojiCount++;
+        Emojis += Emoji(emoji.id);
+      }
+    });
+    let Embed = new Discord.MessageEmbed()
+      .setTitle(`Emojis in ${message.guild.name}.`)
+      .setDescription(`
+        **Animated [${Animated}]**:\n${EmojisAnimated}\n\n**Standard [${EmojiCount}]**:\n${Emojis}\n\n**All Emoji [${OverallEmojis}]**
+      `)
+      .setColor(`#FF0000`);
+    message.channel.send(Embed);
+  
+}})
+
 client.on("message", message => {
   if (message.content.startsWith(PREFIX + "girl")) {
     let man = [
