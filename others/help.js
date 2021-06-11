d59642eefdcf04fa1d17beec68fcc6176cc056dd
Wolf721 <84997879+Wolf-721@users.noreply.github.com>
2021-06-11 19:01:00 +0300

    Update help.js

--- others/help.js
+++ others/help.js
@@ -19,7 +19,16 @@ module.exports = {
     .setAuthor(`Reyna ✨`, `https://cdn.discordapp.com/attachments/843342058352541706/849755772346302484/PicsArt_05-25-07.10.31.jpg`)
     .setDescription(`
 
+**Invite Reyna ✨ to your server by [click here]()**
 
+
+**To see each part of the commands**
+
+> n!info . n!gifs . n!photo . n!utility . n!game . n!text
+> 
+
+
+**Need more help? Come join our [Server Server]()**
 **Links**
 **[Support](https://discord.gg/DXJVbuuJdq) - [invite](https://discord.com/api/oauth2/authorize?client_id=806840212608909344&permissions=8&scope=bot) - [Vote](https://top.gg/bot/806840212608909344/vote)**
 `)
