40b6978302691605aa79bc48c4a8900903e68698
Wolf721 <84997879+Wolf-721@users.noreply.github.com>
2021-07-01 10:32:40 +0300

    Update invite.js

--- others/invite.js
+++ others/invite.js
@@ -1,26 +1,28 @@
 const { Client, Collection, MessageEmbed } = require(`discord.js`);
-const {
-  PREFIX,
-  approveemoji,
-  denyemoji
+const { 
+  PREFIX, 
 } = require(`../config.json`);
 
+  
+
+
 module.exports = {
-  name: `invite`,
-  description: `Gives you an invite`,
-  aliases: ["add"],
-  cooldown: 3,
-  edesc: "Type this command to get an invite link for the Bot, thanks for every Invite",
-  execute(message, args, client) {
-    //react with approve emoji
-    message.react("<:emoji_4:815583574983966720>");
-    //send the invite embed
-    message.reply(new MessageEmbed()
-    .setThumbnail(`https://cdn.discordapp.com/attachments/796122909533405195/846782460943532042/PicsArt_05-25-07.10.31.jpg`)
-    .setAuthor(`Reyna`, `https://cdn.discordapp.com/attachments/843342058352541706/849755772346302484/PicsArt_05-25-07.10.31.jpg`)
-    .setTitle("\`Reyna Invite your server\`")
-    .setColor("#FF0000")
-    .setDescription("\`Reyna Link\` https://discord.com/api/oauth2/authorize?client_id=806840212608909344&permissions=8&scope=bot")
-    .setFooter("Thank you for invited"));
+  name: "invite",
+  aliases: ["i"],
+  cooldown: 8,
+  description: "**all commands**",
+  execute(message) {
+    let commands = message.client.commands.array();
+
+    let helpEmbed = new MessageEmbed()
+    .setDescription(`
+<:emoji_65:815251304070905857> [Click here](https://discord.com/api/oauth2/authorize?client_id=803965149941071875&permissions=8&scope=bot) **to invite the bot.**
+`)
+
+  
+   .setColor("YELLOW");
+   message.react("<:emoji_74:815251307635015731>")
+    return message.channel.send(helpEmbed).catch(console.error);
+
   }
-}
+};
