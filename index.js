928bacae038abf9d588feb9f79899959c7262347
Wolf721 <84997879+Wolf-721@users.noreply.github.com>
2021-07-01 09:54:16 +0300

    Update index.js

--- index.js
+++ index.js
@@ -37,9 +37,9 @@ client.on(`ready`, () => {
       });
       
 
-    client.user.setActivity(`${PREFIX}help`, { type: "LISTENING"});
+    client.user.setActivity(`${PREFIX}help`, { type: "COMPETING"});
 
-    client.user.setActivity(`${client.guilds.cache.size} Server,Users ${client.guilds.cache.reduce((a, g) => a + g.memberCount, 0)},`, { type: "LISTENING"});
+    client.user.setActivity(`${client.guilds.cache.size} Server,Users ${client.guilds.cache.reduce((a, g) => a + g.memberCount, 0)},`, { type: "COMPETING"});
 
     
    
