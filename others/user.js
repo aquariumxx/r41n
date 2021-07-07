d368e0cf5dab2a1697620a9f36781f11693663e6
Wolf721 <84997879+Wolf-721@users.noreply.github.com>
2021-07-07 10:10:39 +0300

    Update user.js

--- others/user.js
+++ others/user.js
@@ -15,7 +15,7 @@ module.exports = {
     let commands = message.client.commands.array();
 
     let helpEmbed = new MessageEmbed()
-    .setTitle(`**Reyna Help**`)
+    .setTitle(`**Reyna User**`)
     .setDescription(`
 
 **User Commands**
