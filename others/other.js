2d7f770cf58399bbafa2c0c61005d8897cad8219
Wolf721 <84997879+Wolf-721@users.noreply.github.com>
2021-07-07 10:08:33 +0300

    Update other.js

--- others/other.js
+++ others/other.js
@@ -15,7 +15,6 @@ module.exports = {
     let commands = message.client.commands.array();
 
     let helpEmbed = new MessageEmbed()
-    .setThumbnail(`https://cdn.discordapp.com/attachments/796122909533405195/846782460943532042/PicsArt_05-25-07.10.31.jpg`)
     .setTitle(`**Reyna Help**`)
     .setDescription(`
 
@@ -31,8 +30,6 @@ module.exports = {
 > \`.create-channel \`
 > \`.delete-channel \`
 > \`.slap\` - \`.hug\`
-**Links**
-**[Support](https://discord.gg/DXJVbuuJdq) - [invite](https://discord.com/api/oauth2/authorize?client_id=806840212608909344&permissions=8&scope=bot)**
 `)
 
    .setFooter(`Requested by ${message.author.username}#${message.author.discriminator}`, message.member.user.displayAvatarURL({ dynamic: true }))
