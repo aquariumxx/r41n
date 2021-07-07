6ad2ccaa1e8573bb922a3e517c36934acd4da401
Wolf721 <84997879+Wolf-721@users.noreply.github.com>
2021-07-07 10:07:36 +0300

    Update help.js

--- others/help.js
+++ others/help.js
@@ -15,16 +15,13 @@ module.exports = {
     let commands = message.client.commands.array();
 
     let helpEmbed = new MessageEmbed()
-    .setThumbnail(`https://cdn.discordapp.com/attachments/796122909533405195/846782460943532042/PicsArt_05-25-07.10.31.jpg`)
-    .setAuthor(`Reyna ✨`, `https://cdn.discordapp.com/attachments/843342058352541706/849755772346302484/PicsArt_05-25-07.10.31.jpg`)
+    .setAuthor(`Reyna`)
     .setTitle(`**To see each part of the commands**`)
     .setDescription(`
 
 > \`.user\` - \`.music\`
 > \`.other\` - \`.gifs\`
 > \`.photo\`
-**Links
-[Invite](https://discord.com/api/oauth2/authorize?client_id=806840212608909344&permissions=8&scope=bot) - [Support](https://discord.gg/brave)**
 `)
 
    .setFooter(`Requested by ${message.author.username}#${message.author.discriminator}`, message.member.user.displayAvatarURL({ dynamic: true }))
