5ada64926f0d2d560d5bc1661e79bd2e4c3054b5
Wolf721 <84997879+Wolf-721@users.noreply.github.com>
2021-07-25 23:19:06 +0300

    Update botinfo.js

--- Music/botinfo.js
+++ Music/botinfo.js
@@ -8,14 +8,16 @@ module.exports = {
   async execute(message, args, client) {
     let embed = new discord.MessageEmbed()
       .setTitle(`Info Reyna Bot`)
-      .addField("Bot Name", `<@${client.user.id}>`)
+      .addField("My Name", `<@${client.user.id}>`)
+      .addField("My ID", ` ${client.user.id} `, true)
       .addField("Libary", `discord.js`)
       .addField("Discord.js Version", `12.5.3`)
+      .addField('My Ping' , `${client.ws.ping}` , true)
       .addField("Guilds", `${client.guilds.cache.size}`, true)
       .addField("Creator", `<@790233637580832788>`)
       .addField("Helper", `<@690934381597949955>`)
       .setColor("#0d2943")
-      .setFooter(`information about bot`);
+      .setFooter(`${message.author.username}#${message.author.discriminator}`, message.member.user.displayAvatarURL({ dynamic: true }));
       message.lineReplyNoMention(embed).catch(console.error);
   }
 };
