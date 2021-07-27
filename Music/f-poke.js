f1d0d40a2658464a1c9c72cc1478a745cdabf750
Wolf721 <84997879+Wolf-721@users.noreply.github.com>
2021-07-27 21:41:21 +0300

    Update f-poke.js

--- Music/f-poke.js
+++ Music/f-poke.js
@@ -1,6 +1,7 @@
 const Discord = require("discord.js");
-const sakran = require('nekos.life');
-const neko = new sakran();
+const { lineReply } = require("discord-reply");
+const wolf = require('nekos.life');
+const neko = new wolf();
 
 module.exports = {
   name: `poke`,
@@ -12,7 +13,7 @@ module.exports = {
 
     const user = message.mentions.users.first();
         if(!user)
-        return message.reply('Mention someone to poke');
+        return message.reply('**Mention someone to poke**');
 
         async function work() {
         let owo = (await neko.sfw.poke());
@@ -21,9 +22,9 @@ module.exports = {
         .setTitle(user.username + " You have been poked! ")
         .setDescription((user.toString() + " got poked by " + message.author.toString()))
         .setImage(owo.url)
-        .setColor(`BLACK`)
+        .setColor(`#FF0000`)
         .setURL(owo.url);
-        message.channel.send(kissembed);
+        message.lineReplyNoMention(pokeembed).catch(console.error);
 
 }
 
