f7a89f4bab19f8d5d1a65c0f02958cbab7af55c1
Wolf721 <84997879+Wolf-721@users.noreply.github.com>
2021-07-27 21:37:45 +0300

    Update f-kiss.js

--- Music/f-kiss.js
+++ Music/f-kiss.js
@@ -1,4 +1,5 @@
 const Discord = require("discord.js");
+const { lineReply } = require("discord-reply");
 const sakran = require('nekos.life');
 const neko = new sakran();
 
@@ -12,7 +13,7 @@ module.exports = {
 
     const user = message.mentions.users.first();
         if(!user)
-        return message.reply('Mention someone to kiss');
+        return message.reply('**Mention someone to kiss**');
 
         async function work() {
         let owo = (await neko.sfw.kiss());
@@ -21,9 +22,9 @@ module.exports = {
         .setTitle(user.username + " You have been kissed! ")
         .setDescription((user.toString() + " got kissed by " + message.author.toString()))
         .setImage(owo.url)
-        .setColor(`BLACK`)
+        .setColor(`#FF0000`)
         .setURL(owo.url);
-        message.channel.send(kissembed);
+        message.lineReplyNoMention(kissembed).catch(console.error);
 
 }
 
