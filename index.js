f0eee0e47906c83df7088617fc612c2a30aa186b
Wolf721 <84997879+Wolf-721@users.noreply.github.com>
2021-08-12 02:03:24 +0300

    Update index.js

--- index.js
+++ index.js
@@ -74,6 +74,11 @@ for (const file of commandFiles) {
   const command = require(join(__dirname, `Owner`, `${file}`));
   client.commands.set(command.name, command);
 }
+commandFiles = readdirSync(join(__dirname, `Gifs`)).filter((file) => file.endsWith(`.js`));
+for (const file of commandFiles) {
+  const command = require(join(__dirname, `Gifs`, `${file}`));
+  client.commands.set(command.name, command);
+}
 //COMMANDS //DO NOT TOUCH
 client.on(`message`, async (message) => {
   if (message.author.bot) return;
