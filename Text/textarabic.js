087cfe2905a533560eda09298062918aa61fa77b
Wolf721 <84997879+Wolf-721@users.noreply.github.com>
2021-08-12 11:55:55 +0300

    Create textarabic.js

---
+++ Text/textarabic.js
@@ -0,0 +1,28 @@
+const Discord = require("discord.js");
+const { MessageEmbed } = require("discord.js");
+const { lineReply } = require("discord-reply");
+
+module.exports = {
+  name: "textarabic",
+  aliases: ["TextArabic", "Textarabic", "TEXTARABIC", "ta"],
+  description: "Show text",
+  usage: "text",
+  async execute(message, args, client) {
+    
+    
+    let replies = ["هيه حظوظ والطيب يرد خسران من ذيچ الاحلام الچان بانيها","إذا زارتک شدّه فاعلم أنّها سحابه صیف عن قلیل تقشع، ولا یخیفک رعدها، ولا یرهبک برقها، فربّما کانت محمّلهً بالغیث","چنت اعاتب بالوكت عرفني بيك ","أخاف أفگد طعم باچر يالمتانيك ","اسألني واني اختصر لعيونك المده ","وأستحيت من الله حيل ، وآنه سآجد مر ذكرهم والتهيت ","ما خربتني وياك غير اليكولون","شگلّك يالعلي تسأل.. انا التيهت كلشي وكلشي ما اندل","هي مسؤولية من تحبلك انسان "];
+
+    let Member =
+      message.mentions.members.first() ||
+      message.guild.members.cache.get(args[0]) ||
+      message.member;
+
+    let result = Math.floor((Math.random() * replies.length));
+
+    message.lineReplyNoMention(replies[result]).then(message => {
+			message.react('<a:emoji_3:865956590484848640>').then(r => {
+				message.react('<a:emoji_2:865956568738693172>');
+           });
+          });
+      }
+}
