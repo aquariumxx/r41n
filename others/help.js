8beb8a35dc53554883619786ab825e14f8574350
Wolf721 <84997879+Wolf-721@users.noreply.github.com>
2021-08-17 13:39:45 +0300

    Update help.js

--- others/help.js
+++ others/help.js
@@ -16,11 +16,10 @@ module.exports = {
     .addField("General Commands", `\`help\`, \`ping\`, \`prefix\`, \`uptime\`, \`avatar\`, \`status\`, \`invites\`, \`suggest\`, \`userinfo\`, \`serverinfo\`, \`botinfo\``)
     .addField("Music Commands", `\`filter\`, \`loop\`, \`lyrics\`, \`np\`, \`pause\`, \`play\`, \`queue\`, \`radio\`, \`remove\`, \`resume\`, \`search\`, \`shuffle\`, \`skip\`, \`skipto\`, \`stop\`, \`volume\``)
     .addField("Moderation Commands", `\`kick\`, \`ban\`, \`mute\`, \`unmute\`, \`clear\`, \`lock\`, \`unlock\`, `)
-    .addField("Fun Commands", `\`slap\`, \`hug\`, \`poke\`,‌ \`pat\`, \`cuddle\`, \`feed\`, \`kiss\``)
-    .addField("Text Commands", `\`textkurdish\`, \`textarabic\`, \`textenglish\`, \`textturkish\`, \`textpersian\``)
-    .addField("Gif Commands", `\`boygif\`, \`girlgif\`, ‌\`couplegif\`, \`animegif\`, \`emojigif\`, \`animalgif\``)
-    .addField("Banner Commands", `\`bboy\`, \`bgirl\`, ‌\`banime\``)
-    .addField("Games Commands", `\`rps\`, \`iq\`, ‌\`howgay\`, \`8ball\`, \`rate\`, \`dicksize\``)
+    .addField("Fun Commands", `\`slap\`, \`hug\`, \`poke\`, \`pat\`, \`cuddle\`, \`feed\`, \`kiss\``)
+    .addField("Gif Commands", `\`boygif\`, \`girlgif\`, \`couplegif\`, \`animegif\`, \`emojigif\`, \`animalgif\``)
+    .addField("Banner Commands", `\`bboy\`, \`bgirl\`, \`banime\``)
+    .addField("Games Commands", `\`rps\`, \`iq\`, \`howgay\`, \`8ball\`, \`rate\`, \`dicksize\``)
     helpEmbed.setTimestamp();
 
     message.lineReplyNoMention(helpEmbed).catch(console.error);
