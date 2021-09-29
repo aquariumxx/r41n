3e7de0a51ea9391087388f6d87c6db8248c0859d
Unknown <83038940+kakUnknownnnnn@users.noreply.github.com>
2021-09-29 18:44:01 +0300

    Update avatar.js

--- Music/avatar.js
+++ Music/avatar.js
@@ -10,7 +10,7 @@ module.exports = {
         const member = message.mentions.members.first() || message.guild.members.cache.get(args[0]) || message.guild.members.cache.find(r => r.user.username.toLowerCase() === args.join(' ').toLocaleLowerCase()) || message.guild.members.cache.find(r => r.displayName.toLowerCase() === args.join(' ').toLocaleLowerCase()) || message.member;
     const embed = new Discord.MessageEmbed()
         .setAuthor(member.user.tag, member.user.displayAvatarURL({ dynamic: true, size: 1024 }))
-        .setColor("#FF0000")
+        .setColor("#00DAFF")
         .setTitle(`Link as`)
         .setDescription(`[png](${member.user.displayAvatarURL({format: "png", size: 1024})}) | [jpg](${member.user.displayAvatarURL({format: "jpg", size: 1024})}) | [webp](${member.user.displayAvatarURL({format: "webp", size: 1024, dynamic: true})})`)
         .setImage(member.user.displayAvatarURL({ dynamic: true, size: 1024 }))
