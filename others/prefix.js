646f71f3a087434db6160309712a06a5cb36fc1b
Unknown <83038940+kakUnknownnnnn@users.noreply.github.com>
2021-09-29 18:43:39 +0300

    Update prefix.js

--- others/prefix.js
+++ others/prefix.js
@@ -23,23 +23,23 @@ module.exports = {
     message.react("<:emoji_4:815583574983966720>");
 
     if(!args[0]) return message.channel.send(new MessageEmbed()
-    .setColor("#FF0000")
+    .setColor("#00DAFF")
     .setTitle(`Current Prefix: \`${prefix}\``)
     .setFooter('Please provide a new prefix')
     );
     if(!message.member.hasPermission("ADMINISTRATOR")) return message.reply(new MessageEmbed()
-    .setColor("#FF0000")
+    .setColor("#00DAFF")
     .setTitle(`You don\'t have permission for this Command!`)
     );
 
     if(args[1]) return message.channel.send(new MessageEmbed()
-    .setColor("#FF0000")
+    .setColor("#00DAFF")
     .setTitle(`'The prefix can\'t have two spaces'`));
 
     db.set(`prefix_${message.guild.id}`, args[0])
 
     message.channel.send(new MessageEmbed()
-    .setColor("#FF0000")
+    .setColor("#00DAFF")
     .setTitle(`Successfully set new prefix to **\`${args[0]}\`**`))
   }
 }
