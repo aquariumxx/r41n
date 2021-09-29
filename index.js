b6f728c2df8f0e00630dd7b15603740254d1eb4b
Unknown <83038940+kakUnknownnnnn@users.noreply.github.com>
2021-09-29 18:06:50 +0300

    Update index.js

--- index.js
+++ index.js
@@ -37,7 +37,7 @@ client.on(`ready`, () => {
       });
       
 
-    client.user.setActivity(`${PREFIX}help | ${client.guilds.cache.size} Guilds`, { type: "COMPETING"});
+    client.user.setActivity(`Comming soon | ${client.guilds.cache.size} Guilds`, { type: "COMPETING"});
 
     
    
