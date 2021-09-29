a9385eb7ccce5860a7903006b348bdfd80d8989e
Unknown <83038940+kakUnknownnnnn@users.noreply.github.com>
2021-09-29 18:13:46 +0300

    Update botinfo.js

--- Music/botinfo.js
+++ Music/botinfo.js
@@ -7,15 +7,15 @@ module.exports = {
   description: "INVITE BOT",
   async execute(message, args, client) {
     let embed = new discord.MessageEmbed()
-      .setTitle(`Info Reyna Bot`)
-      .addField("My Name", `<@${client.user.id}>`)
-      .addField("My ID", ` ${client.user.id} `, true)
-      .addField("Libary", `discord.js`)
-      .addField("Discord.js Version", `12.5.3`)
-      .addField('My Ping' , `${client.ws.ping}` , true)
+      .setAuthor("BlueLine","https://media.discordapp.net/attachments/857669210285146112/892779923742285834/20210929_170621.jpg","https://discord.gg/zFGGTuEmUD")
+      .addField("Name", `<@${client.user.id}>`)
+      .addField("ID", ` ${client.user.id} `, true)
+
+      .addField("Version", `12.5.3`)
+      .addField('Ping' , `${client.ws.ping}` , true)
       .addField("Guilds", `${client.guilds.cache.size}`, true)
-      .addField("Creator", `<@790233637580832788>`)
-      .setColor("#FF0000")
+      .addField("Developer", `UnKnown#0069`)
+      .setColor("#00DAFF")
       .setFooter(`${message.author.username}#${message.author.discriminator}`, message.member.user.displayAvatarURL({ dynamic: true }));
       message.lineReplyNoMention(embed).catch(console.error);
   }
