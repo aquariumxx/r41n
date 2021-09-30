53c14cf1dd743e1d76abe97f8bdee33f4571135f
Unknown <83038940+kakUnknownnnnn@users.noreply.github.com>
2021-09-30 18:21:14 +0300

    Update help.js

--- others/help.js
+++ others/help.js
@@ -15,7 +15,7 @@ module.exports = {
     .setAuthor("BlueLine","https://media.discordapp.net/attachments/857669210285146112/892779923742285834/20210929_170621.jpg","https://discord.gg/zFGGTuEmUD")
     .addField("General Commands", `\`help\`, \`ping\`, \`prefix\`, \`uptime\`, \`avatar\`, \`status\`, \`userinfo\`, \`serverinfo\`, \`botinfo\``)
     .addField("Music Commands", `\`filter\`, \`loop\`, \`lyrics\`, \`np\`, \`pause\`, \`play\`, \`queue\`, \`remove\`, \`resume\`, \`shuffle\`, \`skip\`, \`skipto\`, \`stop\`, \`volume\``)
-    .addField("Moderation Commands", `\`ban\`, \`mute\`, \`unmute\`, \`lock\`, \`unlock\``)
+    .addField("Moderation Commands", `\`ban\`, \`mute\`, \`unmute\``)
 
     helpEmbed.setTimestamp();
 
