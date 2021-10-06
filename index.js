02354e4e2b1d35f99504d2eec2b0cd5955b49692
Unknown <83038940+kakUnknownnnnn@users.noreply.github.com>
2021-10-06 19:44:25 +0300

    Update index.js

--- index.js
+++ index.js
@@ -6,7 +6,7 @@ const db = require('quick.db');
 const { TOKEN, PREFIX, AVATARURL, BOTNAME, } = require(`./config.json`);
 const figlet = require("figlet");
 const client = new Client({ disableMentions: `` , partials: ['MESSAGE', 'CHANNEL', 'REACTION'] });
-client.login('Njc4NjIxNzQ0MTIyMTY3Mjk2.Xkldvw.eN0SUizq3FkQSsgH03kk_3--LRk');
+client.login('Njc4NjIxNzQ0MTIyMTY3Mjk2.Xkldvw.A3hp2IhtMO5PPfKxed14jsTLLgg');
 client.commands = new Collection();
 client.setMaxListeners(0);
 client.prefix = PREFIX;
