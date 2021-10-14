d7dc2c916834bc1bba14f7fdc441f60322f1ab12
Unknown <83038940+kakUnknownnnnn@users.noreply.github.com>
2021-10-14 19:14:41 +0300

    Update ping.js

--- others/ping.js
+++ others/ping.js
@@ -15,6 +15,6 @@ module.exports = {
     //react with approve emoji
     message.react("<:emoji_4:815583574983966720>");
     //send the Ping embed
-    message.reply(new MessageEmbed().setColor("#00DAFF").setTitle("`" + client.ws.ping + "ms`"));
+    message.reply(new MessageEmbed().setColor("#002EFF").setTitle("" + client.ws.ping + "Ping"));
   }
 }
