240a4005261a3b354a1227d5772556be75828af2
Unknown <83038940+kakUnknownnnnn@users.noreply.github.com>
2021-10-14 19:17:50 +0300

    Update botinfo.js

--- Music/botinfo.js
+++ Music/botinfo.js
@@ -7,15 +7,15 @@ module.exports = {
   description: "INVITE BOT",
   async execute(message, args, client) {
     let embed = new discord.MessageEmbed()
-      .setAuthor("BlueLine","https://media.discordapp.net/attachments/857669210285146112/892779923742285834/20210929_170621.jpg","https://discord.gg/zFGGTuEmUD")
-      .addField("Name", `BlueLine Music#5456`)
-      .addField("ID", ` ${client.user.id} `, true)
+      .setAuthor("BlueLine","https://media.discordapp.net/attachments/857669210285146112/898197823026262016/BlueLineMusic.jpg","https://discord.gg/zFGGTuEmUD")
+      .addField("Name", `BlueLineMusic#2395`)
+
 
       .addField("Version", `12.5.3`)
       .addField('Ping' , `${client.ws.ping}` , true)
       .addField("Guilds", `${client.guilds.cache.size}`, true)
-      .addField("Developer", `UnKnown#4354`)
-      .setColor("#00DAFF")
+      .addField("Creator", `UnKnown#4354`)
+      .setColor("#002EFF")
       .setFooter(`${message.author.username}#${message.author.discriminator}`, message.member.user.displayAvatarURL({ dynamic: true }));
       message.lineReplyNoMention(embed).catch(console.error);
   }
