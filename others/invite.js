7c4456e26f11ba52d62213ad7536d2d4f1bf666e
Unknown <83038940+kakUnknownnnnn@users.noreply.github.com>
2021-10-14 18:28:51 +0300

    Update invite.js

--- others/invite.js
+++ others/invite.js
@@ -17,8 +17,8 @@ module.exports = {
     let helpEmbed = new MessageEmbed()
     .setThumbnail("https://media.discordapp.net/attachments/857669210285146112/892779923742285834/20210929_170621.jpg")
     .setDescription("**My Invite Links**")
-    .setAuthor("BlueLine","https://media.discordapp.net/attachments/857669210285146112/892779923742285834/20210929_170621.jpg","https://discord.gg/zFGGTuEmUD")
-    .addField("Invite Me", "[Click](https://discord.com/oauth2/authorize?client_id=678621744122167296&scope=bot%20applications.commands&permissions=8)")
+    .setAuthor("BlueLine","https://media.discordapp.net/attachments/857669210285146112/898197823026262016/BlueLineMusic.jpg","https://discord.gg/zFGGTuEmUD")
+    .addField("Invite Me", "[Click](https://discord.com/oauth2/authorize?client_id=712414335728615454&scope=bot%20applications.commands&permissions=8)")
     .addField("Support", "[Click](https://discord.gg/zFGGTuEmUD)")
     .setTimestamp()
 
