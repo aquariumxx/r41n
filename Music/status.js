d9c80f0ee5cecd1c06a67d62483666c33f948dd7
Unknown <83038940+kakUnknownnnnn@users.noreply.github.com>
2021-10-14 19:38:32 +0300

    Update status.js

--- Music/status.js
+++ Music/status.js
@@ -3,15 +3,18 @@ const { lineReply } = require("discord-reply");
 
 module.exports = {
   name: "status",
-  aliases: ["st"],
+  aliases: ["st","stats"],
   description: "Show status of akeno",
   usage: "status",
   async execute(message, args, client) {
     const embed = new Discord.MessageEmbed()
         .setColor("#00DAFF")
-        .addField("Guilds", `${client.guilds.cache.size}`, true)
-        .addField("Users", `${client.guilds.cache.reduce((a, g) => a + g.memberCount, 0)}`, true)
-        .addField("Channels", `${client.channels.cache.size}`, true)
+        .addField("• Guilds", `${client.guilds.cache.size}`, true)
+        .addField("• Users", `${client.guilds.cache.reduce((a, g) => a + g.memberCount, 0)}`, true)
+        .addField("• Channels", `${client.channels.cache.size}`, true)
+        .addField(`• Voice Connections`, `${client.voice.connections.size}`, false)
+        .addField(`• Uptime`, uptime.join(", "), false)
+        .addField("• Prefix", "u! or custom")
         .setFooter(message.author.username, message.author.avatarURL);
     message.lineReplyNoMention(embed);
 }
