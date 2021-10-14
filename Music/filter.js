4b178d13b58f5d18366a9d56db46f46339653606
Unknown <83038940+kakUnknownnnnn@users.noreply.github.com>
2021-10-14 19:24:08 +0300

    Update filter.js

--- Music/filter.js
+++ Music/filter.js
@@ -116,7 +116,7 @@ async execute(message, args, client) {
       const song = queue.songs[0];
       //play the collected song song, message, client, filters
       message.channel.send(new MessageEmbed()
-      .setColor("#00DAFF")
+      .setColor("#002EFF")
       .setAuthor("Applying: " + args[0], "")).then(msg =>{
         msg.delete({timeout: 2000});
       })
