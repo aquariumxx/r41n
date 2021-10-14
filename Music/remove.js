e665cf81461793ec5a7bc58afca70294a96721d1
Unknown <83038940+kakUnknownnnnn@users.noreply.github.com>
2021-10-14 19:29:45 +0300

    Update remove.js

--- Music/remove.js
+++ Music/remove.js
@@ -35,7 +35,7 @@ execute(message, args) {
     //send approve
     queue.textChannel.send(new MessageEmbed()
     .setDescription(`${message.author} removed **${song[0].title}** from the Queue`)
-    .setColor("#00DAFF")
+    .setColor("#002EFF")
     );
   }
 };
