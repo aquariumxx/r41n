e1dbfc43061149635dfdea5abeec2943aee31966
Unknown <83038940+kakUnknownnnnn@users.noreply.github.com>
2021-10-14 19:34:03 +0300

    Update status.js

--- Music/status.js
+++ Music/status.js
@@ -9,9 +9,9 @@ module.exports = {
   async execute(message, args, client) {
     const embed = new Discord.MessageEmbed()
         .setColor("#00DAFF")
-        .addField("**Servers :**", `\`${client.guilds.cache.size}\``, true)
-        .addField("**Users :**", `\`${client.guilds.cache.reduce((a, g) => a + g.memberCount, 0)}\``, true)
-        .addField("**Channels :**", `\`${client.channels.cache.size}\``, true)
+        .addField("Guilds", `${client.guilds.cache.size}`, true)
+        .addField("Users", `${client.guilds.cache.reduce((a, g) => a + g.memberCount, 0)}`, true)
+        .addField("Channels", `${client.channels.cache.size}`, true)
         .setFooter(message.author.username, message.author.avatarURL);
     message.lineReplyNoMention(embed);
 }
