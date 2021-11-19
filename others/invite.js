7135a6991b026d93caef077cc40c3d64a1b51761
UnKnown <83038940+kakUnknownnnnn@users.noreply.github.com>
2021-11-19 17:51:56 +0300

    Update invite.js

--- others/invite.js
+++ others/invite.js
@@ -16,14 +16,14 @@ module.exports = {
 
     let helpEmbed = new MessageEmbed()
     .setThumbnail("https://cdn.discordapp.com/avatars/712414335728615454/e0e64a900ad2587fb899f1c189d10da6.png?size=1024")
-    .setDescription("**My Invite Links**")
+    .setDescription("**Flower Invite Links**")
     .setAuthor("Flower Bot","https://cdn.discordapp.com/avatars/712414335728615454/e0e64a900ad2587fb899f1c189d10da6.png?size=1024","https://discord.gg/jNuC4n82QF")
     .addField("Invite Me", "[Click](https://discord.com/api/oauth2/authorize?client_id=712414335728615454&permissions=8&scope=bot)")
     .addField("Support", "[Click](https://discord.gg/jNuC4n82QF)")
     .setTimestamp()
 
   
-   .setColor("#002EFF");
+   .setColor("#FF0000");
    message.react("<:emoji_4:815583574983966720>")
     return message.channel.send(helpEmbed).catch(console.error);
 
