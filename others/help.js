702c5ccf265fcb540321ebf728244ffc92464b9d
R4MOx <87250906+r4mox@users.noreply.github.com>
2021-11-22 15:40:41 +0300

    Update help.js

--- others/help.js
+++ others/help.js
@@ -12,7 +12,7 @@ module.exports = {
     .setFooter(`${message.author.username}#${message.author.discriminator}`, message.member.user.displayAvatarURL({ dynamic: true }))
     .setColor(`#FF0000`)
     .setThumbnail("https://cdn.discordapp.com/avatars/712414335728615454/e0e64a900ad2587fb899f1c189d10da6.png?size=1024")
-    .setAuthor("Flower Bot","https://cdn.discordapp.com/avatars/712414335728615454/e0e64a900ad2587fb899f1c189d10da6.png?size=1024","https://discord.gg/jNuC4n82QF")
+    .setAuthor("Flower Bot","","https://discord.gg/jNuC4n82QF")
     .addField("🌎 General", `\`help\`, \`ping\`, \`prefix\`, \`avatar\``)
     .addField("🎶 Music", `\`filter\`, \`loop\`, \`lyrics\`, \`np\`, \`play\`, \`queue\`, \`remove\`, \`shuffle\`, \`skip\`, \`skipto\`, \`stop\`, \`volume\``)
     .addField("❗ Info", `\`botinfo\`, \`serverinfo\`, \`userinfo\`, \`uptime\``)
