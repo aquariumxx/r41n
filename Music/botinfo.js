ee2fc40463f9453a98e62015909ec78fe0c288fd
aquariumxx <96602109+aquariumxx@users.noreply.github.com>
2022-02-08 19:19:07 +0300

    Update botinfo.js

--- Music/botinfo.js
+++ Music/botinfo.js
@@ -7,15 +7,15 @@ module.exports = {
   description: "INVITE BOT",
   async execute(message, args, client) {
     let embed = new discord.MessageEmbed()
-      .setAuthor("Flower Bot","https://cdn.discordapp.com/avatars/712414335728615454/e0e64a900ad2587fb899f1c189d10da6.png?size=1024","https://discord.gg/zFGGTuEmUD")
-      .addField("Name", `Flower Bot`)
+      .setAuthor("Bots Information","https://cdn.discordapp.com/avatars/912844825055854623/84ac4c7425a16a0e7b32cd6076dc3e83.png?size=1024","https://discord.gg/zFGGTuEmUD")
+      .addField("Name", `Emily ✨#1372`)
 
 
       .addField("Version", `12.5.3`)
-      .addField(`Megabat`, (process.memoryUsage().rss / 1024 / 1024).toFixed(2) + "MB", false )
+      .addField(`Megabyte`, (process.memoryUsage().rss / 1024 / 1024).toFixed(2) + "MB", false )
       .addField('Ping' , `${client.ws.ping}` , true)
       .addField("Guilds", `${client.guilds.cache.size}`, true)
-      .addField("Creator", `<@727282746614874113>`)
+      .addField("Creator", `<@755566952449310842>`)
       .setColor("#FF0000")
       .setFooter(`${message.author.username}#${message.author.discriminator}`, message.member.user.displayAvatarURL({ dynamic: true }));
       message.lineReplyNoMention(embed).catch(console.error);
