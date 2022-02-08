a58718d354c842f4c3512ee34a83a48929155ec3
aquariumxx <96602109+aquariumxx@users.noreply.github.com>
2022-02-08 16:46:19 +0300

    Create kiss.js

---
+++ Music/kiss.js
@@ -0,0 +1,25 @@
+const { MessageEmbed } = require("discord.js");
+const fetch = require("node-fetch");
+
+module.exports = {
+  name: "kiss",
+  description: "Shows a picture of people kissing",
+  category: "image",
+  async execute(message, args) {
+    const data = await fetch("https://nekos.life/api/kiss").then((res) =>
+      res.json()
+    );
+    const user = message.mentions.users.first() || message.author;
+    const hugged = message.author.id === user.id ? "themselfs" : user.username;
+
+    const embed = new MessageEmbed()
+      .setTitle(`${message.author.username} kissed ${kissed}`)
+      .setFooter(message.author.username)
+      .setColor("#FC00FF")
+      .setDescription(`[Click here if the image failed to load.](${data.url})`)
+      .setImage(`${data.url}`)
+      .setTimestamp();
+
+    message.channel.send(embed);
+  },
+};
