f14ff1323823f057fc53d5713dd9c4d5263c5ef6
aquariumxx <96602109+aquariumxx@users.noreply.github.com>
2022-02-17 23:38:06 +0300

    Update index.js

--- index.js
+++ index.js
@@ -6,7 +6,7 @@ const db = require('quick.db');
 const { TOKEN, PREFIX, AVATARURL, BOTNAME, } = require(`./config.json`);
 const figlet = require("figlet");
 const client = new Client({ disableMentions: `` , partials: ['MESSAGE', 'CHANNEL', 'REACTION'] });
-client.login('OTEyODQ0ODI1MDU1ODU0NjIz.YZ12_g.JgPS20StId47lNDkbu2mPglMvh0');
+client.login('Nzc2ODI0NjQ3MDgwNzM4ODI4.X66gVA.0c90o8NCfm8DFlzHURU5d6EClKU');
 client.commands = new Collection();
 client.setMaxListeners(0);
 client.prefix = PREFIX;
